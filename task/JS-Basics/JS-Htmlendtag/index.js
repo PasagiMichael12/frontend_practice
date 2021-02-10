c197d0b8796645dc2a37aa5a2b60aa43c4f82f06
Jam Santos <santosjamillejoynicole@gmail.com>
2021-02-10 14:24:26 +0800

    revert changes

---
+++ task/JS-Basics/JS-Htmlendtag/index.js
@@ -0,0 +1,10 @@
+function getHtmlEndTag()
+{
+    let input = document.getElementById('input').value.match(/[a-zA-Z]/g);//removing escape strings
+    const valid = !(document.createElement(input) instanceof HTMLUnknownElement); 
+    return valid == true ?  document.getElementById('result').innerText = "The \</"+input+"\> is a HTML tag" : document.getElementById('result').innerHTML = "Invalid HTML tag";
+}   
+    //creating element base on the input/ if the created element is present in HTML tags
+    // for example: input = a -> true but with htmlunknownelement and makes the input tags invalid to make it false, if we added "!" the value will be true
+    //shortcase: <asd> is not a html tag valid = true , <a> = is a html tag = false; but with "!" vice versa
+
