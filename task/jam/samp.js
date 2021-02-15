2f789c016a97f7236ba53dad68495f008fc5df91
Jam Santos <santosjamillejoynicole@gmail.com>
2021-02-15 10:32:35 +0800

    Array & Functions

--- task/jam/samp.js
+++ task/jam/samp.js
@@ -1,18 +1,31 @@
-var z = 0;
+//a function that calculates the sum of all array inputs
 
 const arraySum = (a) => {
     let sum = 0;
-    for(var z; z < a.length; z++){
+    for(let z = 0; z < a.length; z++){
         sum += a[z];
-        console.log(sum);
     }
-    //return sum;
+    return sum;
     
 }
 
-console.log(arraySum([1,2,3,4,5,6,7,8,9,10]));
+console.log(arraySum([1,2,3,4,5]));
 
 
 let block scope
 var global scope
 const constant
+
+
+//a function that checks the instance of the 2nd number within the 1st parameter (array)
+const numContent = (a, b) => {
+    let val = 0;
+    for(let x = 0; x < a.length; x++){
+        if(a[x] == b){
+            val += 1;
+        }
+    }  
+    return val;
+}
+
+console.log(numContent([42,65,84,83,45,31,10,69,69,70,71,84,84,85], 84));
