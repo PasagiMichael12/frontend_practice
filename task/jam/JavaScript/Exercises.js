42e08e712954f301e9df5d60285aa5b67e223a51
Jam Santos <santosjamillejoynicole@gmail.com>
2021-02-16 00:27:38 +0800

    Array & Function #1-6

--- task/jam/JavaScript/Exercises.js
+++ task/jam/JavaScript/Exercises.js
@@ -1,58 +1,45 @@
-//half pyramid
-const numPyramid = (rows) => {
-    let result = '';
-    for (let x = 1; x <= rows; x++){
-      for (let y = 1; y <= x; y++){
-      result += y + ' ';
-      }
-     console.log(result)
-     result = '';
-     }  
-   }
+//ARRAY & FUNCTION
 
-//closest to 100
-const compare = (num1, num2) => {
-    if (((num1 || num2) > 100) || ((num1 || num2) < 1)){
-         console.log('You entered an invalid number. Please try again.');
-    } else {
-        if (num1 > num2){
-            console.log(`${num1} is the number closest to 100.`);
-        } else {
-            console.log(`${num2} is the number closest to 100.`);
+//#1 Half Pyramid
+const numPyramid = (r) => {
+    let result = '';
+    
+    for (let x = 1; x <= r; x++){
+      
+        for (let y = 1; y <= x; y++){
+            result += y + ' ';
         }
+        
+        console.log(result)
+        result = ''; 
+   }
+}
+
+numPyramid(3);
+
+//#2 Print number closest to 100
+const compare = (num1, num2) => {
+    if (num1 > num2){
+        console.log(`${num1}`);
+    } else {
+        console.log(`${num2}`);
     }
 }
 
-//a function that calculates the sum of all array inputs
+compare(6, 31);
 
+//#3 Sum of all array values
 const arraySum = (a) => {
     let sum = 0;
     for(let z = 0; z < a.length; z++){
         sum += a[z];
     }
-    return sum;
-    
+    console.log(sum);
 }
 
-console.log(arraySum([1,2,3,4,5]));
+arraySum([1,2,3,4,5]);
 
-
-let block scope
-var global scope
-const constant
-Write a function arraySum that accepts a numerically indexed array and returns the sum of the elements. For example,
-// var = arraySum([1, 2, 3, 4, 5]); // output 15
-
-let arraySum = (a) => {
-    let sum = 0;
-    for(let z = 0; z < a.length; z++){
-        sum += a[z];
-    }
-    return sum;
-}
-
-
-//function that checks the occurence of the 2nd number within the 1st parameter (array)
+//#4 Checks the occurence of the 2nd number within the 1st parameter (array)
 const numOccurence = (a, b) => {
     let val = 0;
     for(let x = 0; x < a.length; x++){
@@ -60,13 +47,12 @@ const numOccurence = (a, b) => {
             val += 1;
         }
     }  
-    return val;
+    console.log(val);
 }
 
 numOccurence([1,2,2,2,3,3,5,6], 3);
 
-
-//function that checks the 2nd highest number from the array
+//#5 Checks the 2nd highest number from the array
 const highestNum = (a) => {
     let highest = Math.max(...a);
     let numIndx = a.indexOf(highest);
@@ -81,8 +67,7 @@ const highestNum = (a) => {
 
 console.log(highestNum([1, 10, 30, 45]));
 
-//print array values depending if array[0] is odd/even
-
+//#6 Print array values depending if array[0] is odd/even
 const arrList = (arr) => {
     if(arr[0] % 2 == 0){
         for(let x = 0; x < arr.length; x++){
@@ -99,4 +84,4 @@ const arrList = (arr) => {
     }
 }
 
-arrList([1,2,3,4,5]);
+arrList([,2,3,4,5]);
