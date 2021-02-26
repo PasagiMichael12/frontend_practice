e88f32613cf3b612b26d019e210276ffffca3e53
unknown <santosjamillejoynicole@gmail.com>
2021-02-26 22:42:08 +0800

    .

--- task/jam/JavaScript/JavaScript Exercises/JS14 - Alphabet Sort.js
+++ task/jam/JavaScript/JavaScript Exercises/JS14 - Alphabet Sort.js
@@ -1,2 +1,10 @@
 /*#14 Write a JavaScript function to alphabetize a given string. 
 console.log(arrange('acab')); //aabc */
+
+
+const arrange = (a) => {
+    let arr = a.split("");
+    return arr.sort().join('');
+}
+
+console.log(arrange('acvcdxykhbkhyfdbjicgdsgab'))
