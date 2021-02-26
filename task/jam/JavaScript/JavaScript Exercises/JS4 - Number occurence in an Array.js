5069e93b4bfa18f03233045d7619301be3e485a3
unknown <santosjamillejoynicole@gmail.com>
2021-02-26 11:52:26 +0800

    .

--- task/jam/JavaScript/JavaScript Exercises/JS4 - Number occurence in an Array.js
+++ task/jam/JavaScript/JavaScript Exercises/JS4 - Number occurence in an Array.js
@@ -1,6 +1,5 @@
 //#4 Checks the occurence of the 2nd number within the 1st parameter (array)
 
-//Solution #1
 const numOccurence = (a, b) => {
     let val = 0;
     for(let x = 0; x < a.length; x++){
@@ -13,10 +12,4 @@ const numOccurence = (a, b) => {
 
 numOccurence([1,2,2,2,3,3,5,6], 3);
 
-//Solution #2
-let result = 0;
-let values = [[1,2,3,3,4,4,4,4,4,5], 4];
 
-
-let arrResult = values[0].forEach(newResult);
-console.log(arrResult); 
