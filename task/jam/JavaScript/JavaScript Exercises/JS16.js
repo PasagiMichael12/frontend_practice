28a79b6ee0fbf4a1659ff0c913efedfcb5e056e0
unknown <santosjamillejoynicole@gmail.com>
2021-03-10 16:21:53 +0800

    .

--- task/jam/JavaScript/JavaScript Exercises/JS16.js
+++ task/jam/JavaScript/JavaScript Exercises/JS16.js
@@ -27,16 +27,22 @@ console.log(obj1,obj3) //false: obj1 and obj 3 dont have the same value
 
 //------------------------------------------------
 function Compare(object1, object2) {
-    const obj1 = Object.keys(object1).length;
-    const obj2 = Object.keys(object2).length;
+    // const obj1 = Object.keys(object1).length;
+    // const obj2 = Object.keys(object2).length;
     
     this.try = function(){
-        if (obj1 === obj2) { //if object keys are equal in number
+        if (Object.keys(object1).length === Object.keys(object2).length) { //if object keys are equal in number
 
             //this will return true
-            return Object.keys(obj1).every( 
-                key => obj2.hasOwnProperty(key)  //check obj2 for every key from obj1
-                    && obj2[key] === obj1[key]); //compare values for each key
+            //return Object.keys(obj1).every( 
+                //key => obj2.hasOwnProperty(key)  //check obj2 for every key from obj1
+                   //&& obj2[key] === obj1[key]); //compare values for each key
+            
+            for (keys in object1){
+                if(object2.hasOwnProperty(keys) && object2[keys] === object1[keys]){
+                    return true;
+                }
+            }
         } 
         return false; //if object keys are not equal in number
     }
@@ -46,6 +52,6 @@ const object1 = { a: 1, b: 1 };
 const object2 = { a: 1, b: 1 };
 const object3 = { a: 2, b: 1, c: 3 };
 
-let objCompare = new Compare(object1, object3);
+let objCompare = new Compare(object1, object2);
 console.log(objCompare.try())
 
