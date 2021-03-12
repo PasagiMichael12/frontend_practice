192275ea7191beca199f9ef537b8af8393fcaaf9
Maguszxc <Magtalasallon@gmail.com>
2021-03-12 18:16:21 +0800

    Activity 3 with Evaluation

--- task/JS-Basics/JS-Activity3/index.js
+++ task/JS-Basics/JS-Activity3/index.js
@@ -14,17 +14,23 @@ const Games = [
         status: true
     },
     {
-        title: 'Genshin Impact',
+        title: 'GenshinImpact',
         status: false
     }
 ];
 const getGames = (game, arrOfObj) => {
-    const gameStatus = arrOfObj.filter(obj => obj.status === true && obj.title === game);
+    const gameStatus = arrOfObj.filter(obj => obj.title === game && obj.status === true);
     if (gameStatus.length) {
         console.log(`${gameStatus[0].title} is already played`);
+    } else {
+        console.log(`${game} is not yet played`);
     }
 };
-getGames('Dota2', Games);
+getGames('CSGO', Games);
+/* 
+    Evaluation  = What method to be used. but then i used filter method
+*/
+
 
 /* 2.) Write a function to get the volume of a Cylinder with 2 decimal places using object.
 Volume of a cylinder : V = πr2h
@@ -32,24 +38,17 @@ where r is the radius and h is the height of the cylinder.
 
  ex. var cyl1 = new Cylinder(7,4) // volume = 87.96; */
 //#2
-const shape = {
-    type: '',
-    getType() {
-        return `Shape is ${this.type}`;
-    }
-}
 function Cylinder(height,diameter) {
     this.diameter = diameter;
     this.height = height;
-    this.type = 'Cylinder';
-
 }
-Cylinder.prototype = shape;
-
-shape.getVolumeOfCylinder = function()
+Cylinder.prototype = Cylinder;
+Cylinder.getVolumeOfCylinder = function()
 { 
     let radius = this.diameter/2;
     return console.log('Volume = '+ (Math.PI*radius**2*this.height).toFixed(2));
 }
 let cyl1 = new Cylinder(7,4);
 cyl1.getVolumeOfCylinder();
+
+// Evaluation = I'm confused in making Cylinder constructor to prototype itself.
