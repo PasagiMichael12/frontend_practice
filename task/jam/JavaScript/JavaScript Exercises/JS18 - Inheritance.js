2b4e6bdd9f3ec117c4e8f1c57c8bf33ea7ab20c7
unknown <santosjamillejoynicole@gmail.com>
2021-03-19 14:55:24 +0800

    .

--- task/jam/JavaScript/JavaScript Exercises/JS18 - Inheritance.js
+++ task/jam/JavaScript/JavaScript Exercises/JS18 - Inheritance.js
@@ -55,6 +55,5 @@ return this.a + this.b + this.c;
 
 
 var triangle1 = new Triangle(3, 4, 4);
-console.log(shape.prototype.isPrototypeOf(triangle1));
 console.log(triangle1.getPerimeter());
 console.log(triangle1.getType());
