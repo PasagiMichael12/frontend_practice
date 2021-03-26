08e99c3e902ba7e9e8574bc63d787a8fcc84b009
unknown <santosjamillejoynicole@gmail.com>
2021-03-26 10:38:09 +0800

    .

--- task/jam/DOM/script.js
+++
@@ -1,8 +0,0 @@
-var elementId = document.getElementsByTagName("h1").id;
-
-alert(elementId);
-
-var star = document.getElementById('fallenstar');
-star.remove();
-
-var move = document.getElementById("source")
