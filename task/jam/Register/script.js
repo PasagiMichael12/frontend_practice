33102b2253bf14ebddcf0ed86b6667e633cb687f
unknown <santosjamillejoynicole@gmail.com>
2021-04-06 19:34:10 +0800

    .

--- task/jam/Register/script.js
+++ task/jam/Register/script.js
@@ -9,10 +9,19 @@ function validate(){
     var p1 = document.registrationForm.p1.value; 
     var p2 = document.registrationForm.p2.value; 
     
+    var fields = document.forms["registrationForm"].elements;
+    var submit = true;
 
-    alert("First Name: " + fname + "\nLast Name: " + lname + "\nUsername: " + uname 
-    + "\nBirthdate: " + bdate + "\nPhone Number: " + phone + "\nAddress: " + address + "\nEmail: " + email
-    + "\n\nTo confirm, click 'ok'");
+    for (var x = 0; x < fields.length; i++) {
+        if (fields[i].value.length == 0){
+            submit = false;
+        }
+    }
+
+    document.getElementById('register').setAttribute("disabled", "disabled") = !submit;
+
+    alert("Registered Successfully. \n\nFirst Name: " + fname + "\nLast Name: " + lname + "\nUsername: " + uname 
+    + "\nBirthdate: " + bdate + "\nPhone Number: " + phone + "\nAddress: " + address + "\nEmail: " + email);
 
 
 }  
