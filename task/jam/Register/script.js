c186dbeb66b591660cae629f3a5121332e3ea6f9
unknown <santosjamillejoynicole@gmail.com>
2021-04-06 20:22:16 +0800

    .

--- task/jam/Register/script.js
+++ task/jam/Register/script.js
@@ -1,27 +1,26 @@
-function validate(){  
-    var fname = document.registrationForm.fname.value; 
-    var lname = document.registrationForm.lname.value; 
-    var uname = document.registrationForm.uname.value; 
-    var bdate = document.registrationForm.bdate.value; 
-    var phone = document.registrationForm.phone.value; 
-    var address = document.registrationForm.address.value;  
-    var email = document.registrationForm.email.value; 
-    var p1 = document.registrationForm.p1.value; 
-    var p2 = document.registrationForm.p2.value; 
-    
-    var fields = document.forms["registrationForm"].elements;
-    var submit = true;
+function dataValidate() {
+    var fname = document.registrationForm.fname.value;
+    var lname = document.registrationForm.lname.value;
+    var uname = document.registrationForm.uname.value;
+    var bdate = document.registrationForm.bdate.value;
+    var phone = document.registrationForm.phone.value;
+    var address = document.registrationForm.address.value;
+    var email = document.registrationForm.email.value;
+    var p1 = document.registrationForm.p1.value;
+    var p2 = document.registrationForm.p2.value;
 
-    for (var x = 0; x < fields.length; i++) {
-        if (fields[i].value.length == 0){
-            submit = false;
-        }
+    alert("Registered Successfully. \n\nFirst Name: " + fname + "\nLast Name: " + lname + "\nUsername: " + uname
+        + "\nBirthdate: " + bdate + "\nPhone Number: " + phone + "\nAddress: " + address + "\nEmail: " + email);
+}
+// var fname = document.getElementById("fname");
+
+function validate(){
+    if((document.getElementById("fname").value !== "" && document.getElementById("lname").value !== "") 
+    && (document.getElementById("uname").value !== "" && document.getElementById("bdate").value !== "")
+    && (document.getElementById("phone").value !== "" && document.getElementById("address").value !== "")
+    && (document.getElementById("email").value !== "" && document.getElementById("p1").value !== "")
+    && (document.getElementById("p2").value !== ""))
+    {
+        document.getElementById("register").removeAttribute("disabled");
     }
-
-    document.getElementById('register').setAttribute("disabled", "disabled") = !submit;
-
-    alert("Registered Successfully. \n\nFirst Name: " + fname + "\nLast Name: " + lname + "\nUsername: " + uname 
-    + "\nBirthdate: " + bdate + "\nPhone Number: " + phone + "\nAddress: " + address + "\nEmail: " + email);
-
-
-}  
+}
