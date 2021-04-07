5247da6e9989fa63c619e6e486cb5fb8e37baf4a
unknown <santosjamillejoynicole@gmail.com>
2021-04-07 13:23:39 +0800

    .

--- task/jam/Register/script.js
+++ task/jam/Register/script.js
@@ -12,14 +12,15 @@ function dataValidate() {
     alert("Registered Successfully. \n\nFirst Name: " + fname + "\nLast Name: " + lname + "\nUsername: " + uname
         + "\nBirthdate: " + bdate + "\nPhone Number: " + phone + "\nAddress: " + address + "\nEmail: " + email);
 }
-// var fname = document.getElementById("fname");
+var emailRx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
 function validate(){
     if((document.getElementById("fname").value !== "" && document.getElementById("lname").value !== "") 
     && (document.getElementById("uname").value !== "" && document.getElementById("bdate").value !== "")
-    && (document.getElementById("phone").value !== "" && document.getElementById("address").value !== "")
-    && (document.getElementById("email").value !== "" && document.getElementById("p1").value !== "")
-    && (document.getElementById("p2").value !== ""))
+    && (document.getElementById("address").value !== "")
+    && (emailRx.test(document.getElementById("email").value) && document.getElementById("p1").value !== "")
+    && (document.getElementById("p2").value !== "") 
+    && document.getElementById("p1").value == document.getElementById("p2").value)
     {
         document.getElementById("register").removeAttribute("disabled");
     }
