f1354046e7970f50588113e8e80862451e7fbc07
unknown <santosjamillejoynicole@gmail.com>
2021-04-08 14:03:11 +0800

    .

--- task/jam/Item/script.js
+++ task/jam/Item/script.js
@@ -1,6 +1,17 @@
 function onSubmit() {
     var item = document.getElementById("items");
-    var selecttedItem = item.options[item.selectedIndex].value;
+    var selectedItem = item.options[item.selectedIndex].value;
     var cxname = document.getElementById("name").value;
-    document.getElementById("print").innerHTML = `Customer: ${cxname}` + "</br>" + `Item Description: ${selecttedItem}`;
+    alert(`Customer: ${cxname}` + "\n" + `Item Description: ${selectedItem}`);
+}
+
+
+function validateValues() {
+    var item = document.getElementById("items");
+    var selectedItem = item.options[item.selectedIndex].value;
+    var cxname = document.getElementById("name").value;
+
+    if(selectedItem !== "" && cxname !== ""){
+        document.getElementById("submit").removeAttribute("disabled");
+    }
 }
