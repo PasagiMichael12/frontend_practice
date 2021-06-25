6c384ecfd8db329835a19ab203db97a8c5b2c1b9
unknown <santosjamillejoynicole@gmail.com>
2021-06-25 18:27:25 +0800

    .

--- task/jam/JavaScript/JavaScript Exercises/JS18 - Important JS snippets.js
+++ task/jam/JavaScript/JavaScript Exercises/JS18 - Important JS snippets.js
@@ -4,3 +4,19 @@
 let names = ["Jack", "Loce", "Alice"];
 console.log(names.sort());
 // [ 'Alice', 'Jack', 'Loce' ]
+
+// How to select a random element
+let items = ["Apple", "Banana", "Mango"];
+let index = Math.floor(Math.random() * items.length);
+console.log(items[index]);
+
+// How to reverse a string
+function reverse(s){
+    return s.split("").reverse().join("");
+}
+console.log(reverse("CodingMaster"));
+
+// How to check if element has a class
+const element = document.querySelector("el");
+element.classList.contains("active");
+// returns true or false
