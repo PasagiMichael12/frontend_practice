ee9d34a51b8a25a59e744d5905c3e367f0de6420
Maguszxc <Magtalasallon@gmail.com>
2021-06-29 10:05:44 +0800

    Activity Co-owning

---
+++ Activity/assets/js/index.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+   $.ajax({
+      dataType: 'json',
+      url: 'JSON/index.json',
+      success: (data) => {
+         $('.customer1').on('click', () => {
+            getDetails(data.customer1)
+         })
+         $('.customer2').on('click', () => {
+            getDetails(data.customer2)
+         })
+         $('.customer3').on('click', () => {
+            getDetails(data.customer3)
+         })
+         function getDetails(values) {
+            $('.frontImg').attr('src', `${values.image}`)
+            $('.phrase').text(values.description)
+            $('.system').text(values.system)
+            $('.month').text(values.Monthly)
+            $('.ems').text(values.ems)
+            $('.total').text(values.total)
+         }
+      },
+      error: () => {
+         console.log('Connection Error')
+      },
+   })
+})
